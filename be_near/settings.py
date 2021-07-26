a4a18160c18ac70b2070c5fc93e3e7756b12799c
gradinarnn <gradinarnn1>
2021-07-26 12:16:23 +0300

    для докера

--- be_near/settings.py
+++ be_near/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'selectel',
         'USER' : 'selectel',
         'PASSWORD' : 'selectel',
-        'HOST' : '127.0.0.1',
+        'HOST' : 'pg_db',
         'PORT' : '5432',
     }
 }
