d6e23893441d6e3f65522e7e6626254c2b197ea2
gradinarnn <gradinarnn1>
2021-07-27 11:51:12 +0300

    для докера с ALLOWED_HOSTS

--- be_near/settings.py
+++ be_near/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-deqw&ur^#h!yws3!^6#hhadj05fge$llt+77yqnk(xpuckjd26
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '80.249.150.167',]
 
 
 # Application definition
@@ -83,16 +83,13 @@ WSGI_APPLICATION = 'be_near.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3.db',
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'selectel',
-    #     'USER' : 'selectel',
-    #     'PASSWORD' : 'selectel',
-    #     'HOST' : '80.249.150.167',
-    #     'PORT' : '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'selectel',
+        'USER' : 'selectel',
+        'PASSWORD' : 'selectel',
+        'HOST' : '80.249.150.167',
+        'PORT' : '5432',
     }
 }
 
