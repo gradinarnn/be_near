627dbbb5c31a7dc05103224d3e2d0715ed4e86a6
gradinarnn <gradinarnn1>
2021-07-30 21:30:00 +0300

    длинна скилов = 150

--- filling_profile/admin.py
+++ filling_profile/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Profile, Skills, Categories
+from .models import Profile, Skills, Categories, Profile_for_Metting, Meet
 
 admin.site.register(Profile)
 admin.site.register(Skills)
 admin.site.register(Categories)
+admin.site.register(Profile_for_Metting)
+admin.site.register(Meet)
+
