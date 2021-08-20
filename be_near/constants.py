2a2f261f6a6da0b686928d5e9416f6bb112cf652
gradinarnn <gradinarnn@gmail.com>
2021-08-20 12:09:45 +0300

    для докера

--- be_near/constants.py
+++ be_near/constants.py
@@ -3,6 +3,6 @@
 
 a ='123osdfw4fvccxs'
 
-bot_token='1865010664:AAHfKCHhUrpqNnW33BeZIYxcyurZFGZCQq4'
+bot_token='1928540975:AAH139PCs2V6VcZwJ6BoemOew2-Mr8EbypE'
 
 host = "http://80.249.150.167:8000"
