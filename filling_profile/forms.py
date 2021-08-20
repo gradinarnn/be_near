c148f559c23ab14c255e25548020a6baaaac787f
gradinarnn <gradinarnn@gmail.com>
2021-08-20 13:12:01 +0300

    для  докера

--- filling_profile/forms.py
+++ filling_profile/forms.py
@@ -5,10 +5,12 @@ from .models import Profile, Skills
 
 class Filling_Profile_form(forms.ModelForm):
 
-    # full_name = forms.CharField(max_length=50)
-    # email = forms.CharField(max_length=50)
+    full_name = forms.CharField(label='full_name',widget=forms.TextInput(attrs={'placeholder':'Имя'}))
+    email = forms.CharField(label='email',widget=forms.TextInput(attrs={'placeholder':'E-mail'}))
+    contacts = forms.CharField(label='contacts',widget=forms.TextInput(attrs={'placeholder':'Telegram'}))
+    language = forms.CharField(label='language',widget=forms.TextInput(attrs={'placeholder':'Язык'}))
 
     class Meta:
         model = Profile
-        fields = ('full_name', 'email','goal','contacts','language')
+        fields = ('full_name', 'email','contacts','language')
 
