a0dbf932500387f1b5071d2e4963b6ac3e706701
gradinarnn <gradinarnn@gmail.com>
2021-08-27 22:38:30 +0300

    для докера

--- filling_profile/views.py
+++ filling_profile/views.py
@@ -407,3 +407,84 @@ class stop_meet_change_partner(APIView):
 
 
 
+
+
+
+def check_meeting_3_day():
+    checking_meeting= CallbackData("first_button", "status")
+    text = f'🙌 Привет! Уже узпел паобщаться с собеседником?'
+    a = InlineKeyboardMarkup(
+        row_width=3,
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text='Да, всё гуд',
+                    callback_data=checking_meeting.new(status="ok_good!"),
+
+                ),
+                InlineKeyboardButton(
+                    text='Нет, ещё не общались',
+                    callback_data=checking_meeting.new(status="not_communicate")
+
+                ),
+                InlineKeyboardButton(
+                    text='Парнёр не отвечает',
+                    callback_data=checking_meeting.new(status="not_answer")
+
+                )
+            ]
+        ]
+    )
+
+    all_active_meets = Meet.objects.all().filter(status='active')
+
+    
+
+    for meets in all_active_meets:
+
+        # Если профиль был удален кем-то и как-то, то это предотвратит ошибку
+        try:
+            first_profile = Profile.objects.get(id=meets.first_profile_id).contacts
+            profile = True
+        except Profile.DoesNotExist:
+            profile = False
+        print(f"*")
+        if profile:
+            url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={first_profile}&text={text}&reply_markup={a}'
+
+            payload = {}
+            headers = {}
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+        profile = False
+        try:
+            second_profile = Profile.objects.get(id=meets.second_profile_id).contacts
+            profile = True
+        except Profile.DoesNotExist:
+            profile = False
+        if profile:
+            url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={second_profile}&text={text}&reply_markup={a}'
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+
+def run_threaded():
+    schedule.every().wednesday.at("11:00").do(check_meeting_3_day,)
+
+
+    while True:  # этот цикл отсчитывает время. Он обязателен.
+        print(f"-")
+        schedule.run_pending()
+        time.sleep(1)
+    
+
+
+
+
+
+job_thread = threading.Thread(target=run_threaded)
+job_thread.start()
+
+
+
+
+
