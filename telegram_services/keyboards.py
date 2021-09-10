2f493b2e033f200e4eab6e1ca7269a6965060023
gradinarnn <gradinarnn@gmail.com>
2021-09-10 21:38:11 +0300

    Refactor кейса Среды

--- telegram_services/keyboards.py
+++ telegram_services/keyboards.py
@@ -1,5 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from filling_profile.CallbackData import meeting_feedback
+
 
 def two_buttons(first_button_text,first_button_callback, second_button_text,second_button_callback):
     return InlineKeyboardMarkup(
@@ -16,6 +18,40 @@ def two_buttons(first_button_text,first_button_callback, second_button_text,seco
                     callback_data=second_button_callback,
 
                 )
+            ]
+        ]
+    )
+
+def leave_feedback_buttons():
+    return InlineKeyboardMarkup(
+        row_width=5,
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text='👎',
+                    callback_data=meeting_feedback.new(status="1")
+
+                ),
+                InlineKeyboardButton(
+                    text='😒',
+                    callback_data=meeting_feedback.new(status="2")
+
+                ),
+                InlineKeyboardButton(
+                    text='🙂',
+                    callback_data=meeting_feedback.new(status="3")
+
+                ),
+
+                InlineKeyboardButton(
+                    text='😍',
+                    callback_data=meeting_feedback.new(status="4")
+                ),
+                InlineKeyboardButton(
+                    text='👍',
+                    callback_data=meeting_feedback.new(status="5")
+                )
+
             ]
         ]
     )
